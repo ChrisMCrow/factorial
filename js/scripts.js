4e5a6fb61c3d8149e17eeef933e2af6deb4bfc01
Chris Crow and Derek Smith <chris@>
2018-08-14 15:37:59 -0700

    add palindrome function

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,22 @@ function factorial(n){
 
 var x = prompt("Enter a number to be put in a factorial");
 alert(factorial(x));
+
+//palindromeprompt-->
+
+
+function palindrome(x) {
+  var xArray = x.split("");
+  var yArray = xArray.reverse()
+  var y = yArray.join("");
+	if (x === y) {
+		alert(true)
+  } else if (x != y) {
+ 		alert(false)
+  }
+}
+
+
+
+var userInput = prompt("does this work?");
+ palindrome(userInput)
